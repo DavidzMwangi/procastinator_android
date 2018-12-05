9671fab7d1a2351e2ea19adf2c28a62c4bc63a20
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-12-05 12:11:28 +0300

    editted changes on the auth module

--- app/src/main/java/com/example/mwang/procastinator/models/access/User.java
+++ app/src/main/java/com/example/mwang/procastinator/models/access/User.java
@@ -1,7 +1,38 @@
 package com.example.mwang.procastinator.models.access;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
 
 @Entity(tableName = "user_table")
 public class User {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    @SerializedName("id")
+    private int id;
+
+    @ColumnInfo(name = "name")
+    @SerializedName("name")
+    public String name;
+
+    @ColumnInfo(name = "email")
+    @SerializedName("email")
+    public String email;
+
+
+
+
+    @NonNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
 }
